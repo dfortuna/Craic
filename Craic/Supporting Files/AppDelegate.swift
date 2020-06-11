eed00dbf576bfe246d1e05f986eaf81de1b540da
dfortuna <denis.fortuna@gmail.com>
2020-06-10 23:05:28 -0300

    Adding Facebook and Firebase SDKs

--- Craic/Supporting Files/AppDelegate.swift
+++ Craic/Supporting Files/AppDelegate.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+
+        ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+        )
+
     }
 
     // MARK: UISceneSession Lifecycle
@@ -31,7 +47,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
